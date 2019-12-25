c7e5bb30ff7092d289f3229081bb8f49ded991c2
blackdevil98 <zinzuvadiyameet98@gmail.com>
2019-12-25 17:58:08 +0530

    Updated vision api

---
+++ main.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const vision = require('@google-cloud/vision');
+
+const app = express();
+
+async function quickstart() {
+    const client = new vision.ImageAnnotatorClient({
+        keyFilename: 'APIkey.json'
+    });
+  
+    // Performs label detection on the image file
+    const [result] = await client.labelDetection('./images/object.jpg');
+    const labels = result.labelAnnotations;
+
+    console.log('Labels:');
+    labels.forEach(label => console.log(label.description));
+
+    console.log("");
+    console.log("Objects");
+
+    const [objresult] = await client.objectLocalization('./images/object.jpg');
+    const objects = objresult.localizedObjectAnnotations;
+    objects.forEach(object => {
+    console.log(`${object.name}`);
+    });
+
+    console.log("");
+    console.log("Logo");
+
+    const [logoresult] = await client.logoDetection('./images/object.jpg');
+    const logos = logoresult.logoAnnotations;
+    console.log('Logos:');
+    logos.forEach(logo => console.log(logo));
+
+    console.log("");
+    console.log("Web");
+
+    const [webresult] = await client.webDetection('./images/object.jpg');
+    const webDetection = webresult.webDetection;
+    // if (webDetection.fullMatchingImages.length) {
+    // console.log(
+    //     `Full matches found: ${webDetection.fullMatchingImages.length}`
+    // );
+    // webDetection.fullMatchingImages.forEach(image => {
+    //     console.log(`  URL: ${image.url}`);
+    //     console.log(`  Score: ${image.score}`);
+    // });
+    // }
+
+    // if (webDetection.partialMatchingImages.length) {
+    // console.log(
+    //     `Partial matches found: ${webDetection.partialMatchingImages.length}`
+    // );
+    // webDetection.partialMatchingImages.forEach(image => {
+    //     console.log(`  URL: ${image.url}`);
+    //     console.log(`  Score: ${image.score}`);
+    // });
+    // }
+
+    if (webDetection.webEntities.length) {
+    webDetection.webEntities.forEach(webEntity => {
+        console.log(`${webEntity.description}`);
+    });
+    }
+
+    // if (webDetection.bestGuessLabels.length) {
+    // console.log(
+    //     `Best guess labels found: ${webDetection.bestGuessLabels.length}`
+    // );
+    // webDetection.bestGuessLabels.forEach(label => {
+    //     console.log(`  Label: ${label.label}`);
+    // });
+    // }
+
+    // console.log("");
+    // console.log("land");
+    // // `gs://${bucketName}/${fileName}`
+    // const [landresult] = await client.landmarkDetection('./images/object.jpg');
+    // const landmarks = landresult.landmarkAnnotations;
+    // console.log('Landmarks:');
+    // landmarks.forEach(landmark => console.log(landmark));
+
+
+
+}
+
+quickstart().catch(console.error);
+
+app.listen(8080, '127.0.0.1', () => console.log("Server is running"));
