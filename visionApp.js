7ceb61943a465083777e6cda334f946e1a1b8b2a
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2020-01-09 23:47:24 +0530

    Added vision API

---
+++ visionApp.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const vision = require("@google-cloud/vision");
+
+const app = express();
+
+async function quickstart() {
+  const client = new vision.ImageAnnotatorClient({
+    keyFilename: "APIkey.json"
+  });
+
+  // Performs label detection on the image file
+  const [result] = await client.labelDetection("./images/object.jpg");
+  const labels = result.labelAnnotations;
+
+  console.log("Labels:");
+  labels.forEach(label => console.log(label.description));
+
+  console.log("");
+  console.log("Objects");
+
+  const [objresult] = await client.objectLocalization("./images/object.jpg");
+  const objects = objresult.localizedObjectAnnotations;
+  objects.forEach(object => {
+    console.log(`${object.name}`);
+  });
+
+  console.log("");
+  console.log("Logo");
+
+  const [logoresult] = await client.logoDetection("./images/object.jpg");
+  const logos = logoresult.logoAnnotations;
+  console.log("Logos:");
+  logos.forEach(logo => console.log(logo));
+
+  console.log("");
+  console.log("Web");
+
+  const [webresult] = await client.webDetection("./images/object.jpg");
+  const webDetection = webresult.webDetection;
+
+  if (webDetection.webEntities.length) {
+    webDetection.webEntities.forEach(webEntity => {
+      console.log(`${webEntity.description}`);
+    });
+  }
+}
+
+quickstart().catch(console.error);
+
+app.listen(8080, "127.0.0.1", () => console.log("Server is running"));
