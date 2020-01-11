b133cdfdc1ac518d7842da02c4e169f583296047
Himanshu72 <36104578+Himanshu72@users.noreply.github.com>
2020-01-11 12:14:59 +0530

    img upload

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
+const busboy = require("connect-busboy");
 const indexRouter = require("./routes/index");
 const visionRouter = require("./routes/vision");
-
+const bodyParser = require("body-parser");
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(busboy());
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(busboy());
 app.use("/", indexRouter);
 app.use("/", visionRouter);
 module.exports = app;
--- routes/vision.js
+++ routes/vision.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const servie = require("../services/service");
 //const vision = require("@google-cloud/vision");
-
+var multer = require("multer");
+var upload = multer({ dest: "./public/images/" });
 const visionRouter = express.Router();
 
-visionRouter.get("/vision/:id", function(req, res, next) {
+visionRouter.post("/vision/:id", upload.single("avatar"), function(
+  req,
+  res,
+  next
+) {
+  console.log();
   let prom;
   if (req.params.id == 1) {
-    prom = servie.sendLable("./public/images/object.jpg");
+    prom = servie.sendLable(`./public/images/${req.file.filename}`);
   } else if (req.params.id == 2) {
-    prom = servie.sendText("./public/images/object.jpg");
+    prom = servie.sendText(`./public/images/${req.file.filename}`);
   } else if (req.params.id == 3) {
-    res.send({ msg: "face recognition under constraction" });
+    res.send({ msg: "face recognition under progress" });
     return;
   } else {
     res.status(400).send({ msg: "something went wrong" });
