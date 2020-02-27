f1ab3a02db2f07397579c7f6f126e96b90badb02
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2020-02-27 19:47:56 +0530

    Python bridge fixed

--- test.js
+++ test.js
@@ -1,16 +1,50 @@
-let pythonBridge = require("python-bridge");
+// Partially Working Script
+// const path = require("path");
+// const { spawn } = require("child_process");
 
-let python = pythonBridge();
+// /**
+//  * Run python script, pass in `-u` to not buffer console output
+//  * @return {ChildProcess}
+//  */
+// function runScript() {
+//   return spawn("python", [
+//     "-u",
+//     path.join(__dirname, "test.py"),
+//     "--foo",
+//     "some value for foo"
+//   ]);
+// }
 
-python.ex`from num2words import num2words`;
-python.ex`from subprocess import call`;
+// const subprocess = runScript();
 
-python`cmd_beg = 'espeak '`;
-python`cmd_end = ' | aplay /home/pi/Desktop/Text.wav  2>/dev/null'`;
-python`cmd_out = '--stdout > /home/pi/Desktop/Text.wav ' `;
-python`text = input("Enter the Text: ")`;
-python`print(text)`;
-python`text = text.replace(' ', '_')`;
-python`call([cmd_beg+cmd_out+text+cmd_end], shell=True)`;
+// // print output of script
+// subprocess.stdout.on("data", data => {
+//   console.log(`${data}`);
+// });
+// subprocess.stderr.on("data", data => {
+//   console.log(`error:${data}`);
+// });
+// subprocess.on("close", () => {
+//   console.log("Closed");
+// });
 
-python.end();
+let { PythonShell } = require("python-shell");
+let pyshell = new PythonShell("test.py");
+
+// PythonShell.run("test.py", null, function(err) {
+//   if (err) throw err;
+//   console.log("finished");
+// });
+
+pyshell.send("hello");
+
+pyshell.on("message", function(message) {
+  // received a message sent from the Python script (a simple "print" statement)
+  console.log(message);
+});
+pyshell.end(function(err, code, signal) {
+  if (err) throw err;
+  console.log("The exit code was: " + code);
+  console.log("The exit signal was: " + signal);
+  console.log("finished");
+});
