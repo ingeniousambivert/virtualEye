480a11abc228512d7ae3fc9af5d61a4150d590da
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2020-02-27 14:43:42 +0530

    Added python bridge

--- test.js
+++ test.js
@@ -1,73 +1,24 @@
-const express = require("express");
-const vision = require("@google-cloud/vision");
+let pythonBridge = require("python-bridge");
 
-const SimpleTTS = require("simpletts");
-const tts = new SimpleTTS();
-const fs = require("fs");
-const { StillCamera } = require("pi-camera-connect");
+let python = pythonBridge();
 
-const runApp = async () => {
-  const stillCamera = new StillCamera();
-  await stillCamera
-    .takeImage()
-    .then(image => {
-      fs.writeFileSync("image.jpg", image);
-      startRecognizing().catch(console.error);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-};
-runApp();
+python.ex`from num2words import num2words`;
+python.ex`from subprocess import call`;
 
-const app = express();
+python`
+cmd_beg = 'espeak '
+# To play back the stored .wav file and to dump the std errors to /dev/null
+cmd_end = ' | aplay /home/pi/Desktop/Text.wav  2>/dev/null'
+cmd_out = '--stdout > /home/pi/Desktop/Text.wav '  # To store the voice file
 
-async function startRecognizing() {
-  const client = new vision.ImageAnnotatorClient({
-    keyFilename: "./configs/APIKeyForCloud.json"
-  });
+text = input("Enter the Text: ")
+print(text)
 
-  // Performs label detection on the image file
-  const [result] = await client.labelDetection("./image.jpg");
-  const labels = result.labelAnnotations;
+# Replacing ' ' with '_' to identify words in the text entered
+text = text.replace(' ', '_')
 
-  console.log("Labels:");
-  str = "There are ";
+# Calls the Espeak TTS Engine to read aloud a Text
+call([cmd_beg+cmd_out+text+cmd_end], shell=True)
+`;
 
-  labels.forEach(label => {
-    console.log(label.description);
-    str += label.description + ",";
-  });
-
-  tts
-    .getVoices()
-    .then(voices => {
-      let voiceValue = 7;
-      tts.read({ text: str, voice: voices[voiceValue] });
-      // console.log(voices[voiceValue].name);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-
-  console.log("");
-  console.log("Objects");
-
-  const [objresult] = await client.objectLocalization("./image.jpg");
-  const objects = objresult.localizedObjectAnnotations;
-  objects.forEach(object => {
-    console.log(`${object.name}`);
-  });
-
-  // console.log("");
-  // console.log("landmark");
-
-  // const [landresult] = await client.landmarkDetection(
-  //   "../public/images/image.jpg"
-  // );
-  // const landmarks = landresult.landmarkAnnotations;
-  // console.log("Landmarks:");
-  // landmarks.forEach(landmark => console.log(landmark));
-
-  console.log("Finished");
-}
+python.end();
