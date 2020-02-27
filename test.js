607d603c925172f0212d431094f13901e1dbe9b9
Monarch Maisuriya <monarchmaisuriya7600@gmail.com>
2020-02-27 19:07:10 +0530

    Added python bridge

--- test.js
+++ test.js
@@ -5,20 +5,12 @@ let python = pythonBridge();
 python.ex`from num2words import num2words`;
 python.ex`from subprocess import call`;
 
-python`
-cmd_beg = 'espeak '
-# To play back the stored .wav file and to dump the std errors to /dev/null
-cmd_end = ' | aplay /home/pi/Desktop/Text.wav  2>/dev/null'
-cmd_out = '--stdout > /home/pi/Desktop/Text.wav '  # To store the voice file
-
-text = input("Enter the Text: ")
-print(text)
-
-# Replacing ' ' with '_' to identify words in the text entered
-text = text.replace(' ', '_')
-
-# Calls the Espeak TTS Engine to read aloud a Text
-call([cmd_beg+cmd_out+text+cmd_end], shell=True)
-`;
+python`cmd_beg = 'espeak '`;
+python`cmd_end = ' | aplay /home/pi/Desktop/Text.wav  2>/dev/null'`;
+python`cmd_out = '--stdout > /home/pi/Desktop/Text.wav ' `;
+python`text = input("Enter the Text: ")`;
+python`print(text)`;
+python`text = text.replace(' ', '_')`;
+python`call([cmd_beg+cmd_out+text+cmd_end], shell=True)`;
 
 python.end();
